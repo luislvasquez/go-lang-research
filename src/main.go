5129de226c180e9cc438b90c82032bbe791b4c85
Leo Vasquez <luis.vasquez@ampcontrol.io>
2022-05-22 14:49:07 -0500

    refactor: Clean whitespaces :broom:

--- src/main.go
+++ src/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 type album struct {
-	ID	string `json:"id"`
+	ID string `json:"id"`
 	Title string `json:"title"`
 	Artist string `json:"artist"`
 	Price float32 `json:"price"`
@@ -14,9 +14,9 @@ type album struct {
 
 var albums = []album {
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-    {ID: "4", Title: "Lord of the Rings", Artist: "J.R.R. Tolkien", Price: 59.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "Lord of the Rings", Artist: "J.R.R. Tolkien", Price: 59.99},
 }
 
 func main() {
